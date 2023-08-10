8b402e4337f3061573f0f0ee9d676bb27f39c486
Ranayke Boni Ferreira <ranayke.ferreira@wedotec.com.br>
2023-08-10 14:10:15 -0300

    Created database filters with JPQL

--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,9 +1,12 @@
 package br.com.alura.loja.testes;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
+import br.com.alura.loja.dao.CategoriaDAO;
+import br.com.alura.loja.dao.ProdutoDAO;
 import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.utils.JPAUtil;
@@ -11,23 +14,36 @@ import br.com.alura.loja.utils.JPAUtil;
 public class CadastroDeProduto {
 	
 	public static void main(String[] args) {
+		cadastrarProduto();
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDAO produtoDao = new ProdutoDAO(em);
+		
+		Produto p = produtoDao.buscarPorId(1l);
+		System.out.println(p.getPreco());
+		
+		List<Produto> todos = produtoDao.buscarPorNomeDaCategoria("CELULARES");
+		todos.forEach(p2 -> System.out.println(p2.getNome()));
+		
+		BigDecimal precoDoProduto = produtoDao.buscarPrecoDoProdutoComNome("Xiaomi Redmi");
+		System.out.println(precoDoProduto);
+		
+	}
+
+	private static void cadastrarProduto() {
 		Categoria celulares = new Categoria("CELULARES");
 		Produto celular = new Produto("Xiaomi Redmi", "Muito bom", new BigDecimal("800"), celulares );
 		
 		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDAO produtoDao = new ProdutoDAO(em);
+		CategoriaDAO categoriaDao = new CategoriaDAO(em);
+		
 		em.getTransaction().begin();
 		
-		em.persist(celulares);
-		celulares.setNome("XPTO");
+		categoriaDao.cadastrar(celulares);
+		produtoDao.cadastrar(celular);
 		
-		em.flush();
-		em.clear();
-		
-		celulares = em.merge(celulares);
-		celulares.setNome("1234");
-		em.flush();
-		em.remove(celulares);
-		em.flush();
+		em.getTransaction().commit();
+		em.close();
 	}
 	
 }
